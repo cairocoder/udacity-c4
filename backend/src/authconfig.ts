be78feb2a5e2b950392393ff835442834a6caa62
CairoCoder <cairocoder@gmail.com>
2021-03-26 05:06:58 +0200

    adding creddentials

---
+++ backend/src/authconfig.ts
@@ -0,0 +1,7 @@
+
+
+const auth_domain = 'cairocoder.us.auth0.com'  // update with AUTH Domain
+
+export const auth_url = `https://${auth_domain}/pem`
+
+
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = '4b5v02k0pl'  // UPDATE API ID
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'cairocoder.us.auth0.com',    // UPDATE AUTH DOMAIN
+  clientId: 'JiRTM3PXwcDdWBbjWoIG2zE8vgSQarbD',  // UPDATE AUTH CLIENT ID
   callbackUrl: 'http://localhost:3000/callback'
 }
